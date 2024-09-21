6c3943911e4ece9d475cd051b9a0b2fe83d8ff57
Andrei Guzga <shhican@gmail.com>
2024-09-22 01:44:06 +0300

    Fixed issue with lualine not using tokyonight colors

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require('lualine').setup {
         options = {
-          theme = 'catppuccin',
+          theme = 'tokyonight',
           section_separators = { '', '' },
           component_separators = { '', '' },
         },

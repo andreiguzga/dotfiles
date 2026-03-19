2b8d52141278401984146c9b5947d3d92d16e17b
Andrei Guzga <shhican@gmail.com>
2026-03-19 11:51:28 +0200

    Added sketchybar configs

---
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FOCUSED_WORKSPACE="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  sketchybar --set "$NAME" background.drawing=on
+else
+  sketchybar --set "$NAME" background.drawing=off
+fi

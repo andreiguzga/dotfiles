fb5abf0d6171d1d4032db5681a6d79809fe6385f
Andrei Guzga <shhican@gmail.com>
2026-03-19 12:41:47 +0200

    More changes to sketchybar and aerospace

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-FOCUSED_WORKSPACE="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
+DISPLAY_ID="$1"
+WORKSPACE_ID="$2"
+VISIBLE_WORKSPACE="$(aerospace list-workspaces --monitor "$DISPLAY_ID" --visible 2>/dev/null | sed -n '1p')"
+NONEMPTY_WORKSPACES="$(aerospace list-workspaces --monitor "$DISPLAY_ID" --empty no 2>/dev/null)"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on
+DRAWING=off
+for sid in $NONEMPTY_WORKSPACES
+do
+  if [ "$sid" = "$WORKSPACE_ID" ]; then
+    DRAWING=on
+    break
+  fi
+done
+
+if [ "$WORKSPACE_ID" = "$VISIBLE_WORKSPACE" ]; then
+  DRAWING=on
+fi
+
+if [ "$WORKSPACE_ID" = "$VISIBLE_WORKSPACE" ]; then
+  sketchybar --set "$NAME" drawing="$DRAWING" background.drawing=on icon.color=0xffe5e9f0
 else
-  sketchybar --set "$NAME" background.drawing=off
+  sketchybar --set "$NAME" drawing="$DRAWING" background.drawing=off icon.color=0xffaeb6c5
 fi
--- sketchybar/.config/sketchybar/plugins/front_app.sh
+++ sketchybar/.config/sketchybar/plugins/front_app.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-# Some events send additional information specific to the event in the $INFO
-# variable. E.g. the front_app_switched event sends the name of the newly
-# focused application in the $INFO variable:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+DISPLAY_ID="$1"
 
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+APP_INFO="$(aerospace list-windows --monitor "$DISPLAY_ID" --format '%{app-name}|%{app-bundle-id}|%{workspace-is-visible}|%{workspace-is-focused}' 2>/dev/null | sed -n '/|true|/p' | sed -n '1p')"
+APP_NAME="$(printf '%s' "$APP_INFO" | cut -d'|' -f1)"
+APP_BUNDLE_ID="$(printf '%s' "$APP_INFO" | cut -d'|' -f2)"
+
+if [ -z "$APP_NAME" ] && [ "$SENDER" = "front_app_switched" ]; then
+  APP_NAME="$INFO"
 fi
+
+if [ -z "$APP_NAME" ]; then
+  APP_NAME="Desktop"
+fi
+
+ICON_SCALE=0.52
+MONITOR_NAME="$(aerospace list-monitors --format '%{monitor-id}|%{monitor-name}' 2>/dev/null | sed -n "/^$DISPLAY_ID|/s/^[^|]*|//p" | sed -n '1p')"
+
+case "$MONITOR_NAME" in
+  *[Bb]uilt-[Ii]n*) ICON_SCALE=0.58 ;;
+esac
+
+if [ -n "$APP_BUNDLE_ID" ]; then
+  APP_ICON="app.$APP_BUNDLE_ID"
+else
+  APP_ICON="app.$APP_NAME"
+fi
+
+sketchybar --set "$NAME" label="$APP_NAME" background.drawing=off icon="" icon.background.drawing=on icon.background.color=0x00000000 icon.background.image="$APP_ICON" icon.background.image.scale="$ICON_SCALE"
